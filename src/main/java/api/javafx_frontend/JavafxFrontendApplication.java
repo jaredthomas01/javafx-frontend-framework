309a76db467d4b47ba6305171fee0a0c5cccb0a6
jaredthomas01 <jaredthomas570j@gmail.com>
2025-01-02 13:55:31 -0500

    first commit

---
+++ src/main/java/api/javafx_frontend/JavafxFrontendApplication.java
@@ -0,0 +1,13 @@
+package api.javafx_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafxFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavafxFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/javafx_frontend/JavafxFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package api.javafx_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafxFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
